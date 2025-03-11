85b59e763bc33ce542afb4505f5bea1ab19a5504
shashiweb <shashiweb64@gmail.com>
2025-03-11 18:47:01 +0530

    create new project

---
+++ src/About.js
@@ -0,0 +1,23 @@
+function Myabout()
+{
+    return <h1>this is first page</h1>
+}
+export default Myabout
+
+
+
+export function Contactus()
+{
+    return <h2>this is heading two</h2>
+}
+
+export function Mypage()
+{
+    return(
+        <div className="page">
+            <h1>this is page heading</h1>
+            <p>slddfh j sdhfjsdfn</p>
+            <section>about page section</section>
+        </div>
+    )
+}
