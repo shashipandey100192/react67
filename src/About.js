a1f7d066bbdd329a59a472f29894449180f31c2d
shashiweb <shashiweb64@gmail.com>
2025-03-20 19:09:22 +0530

    change

--- src/About.js
+++ src/About.js
@@ -13,11 +13,21 @@ export function Contactus()
 
 export function Mypage()
 {
+    const name = "kumar";
+    const y = true;
+    const age = 100;
+    const abc = "default value";
+
     return(
         <div className="page">
             <h1 className="heading">this is page heading</h1>
             <p>slddfh j sdhfjsdfn</p>
-            <section>about page section</section>
+            <section style={{color:'red',fontSize:80,backgroundColor:'orange'}}>about page section</section>
+            <p>this is variable based {name}</p>
+            <p>this is variable based {900+800+700}</p>
+            <p>this is condition dist {y===true? "yes" : "no"}</p>          
+            <p>Age: {age>=800? "first condition" : age>=500? "second condition" : age>=200? "third" : <h1>Fail</h1>}</p> 
+            <input type="text" placeholder="username" value={abc}/>
         </div>
     )
 }
